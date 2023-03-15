f095c1afb7086f84babc78c68d0c423a5f3ff105
build wizard <buildwizard@wizardcontroller.com>
2023-03-15 00:00:22 -0400

    sanity commit

---
+++ HorselessGenericOData.EFCoreFeatures/HorselessDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HorselessGenericOData.EFCoreFeatures
+{
+    public class HorselessDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ HorselessGenericOData.EFCoreFeatures/Infrastructure/EntityRegistrationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorselessGenericOData.EFCoreFeatures.Infrastructure
+{
+    public static class EntityRegistrationExtensions
+    {
+        /// <summary>
+        /// generate dbset properties for the provided types
+        /// filtered by the supplied interface
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <param name="modelBuilder"></param>
+        /// <param name="types"></param>
+        public static void RegisterTypes<TInterface>(this Microsoft.EntityFrameworkCore.ModelBuilder modelBuilder, params Type[] types)
+        {
+            IEnumerable<Type> filteredTypes = types.Where(c => c.IsClass && !c.IsAbstract && c.IsPublic &&
+            typeof(TInterface).IsAssignableFrom(c));
+
+            foreach (Type type in filteredTypes)
+            {
+                try
+                {
+                    modelBuilder.Entity(type);
+                }
+                catch (Exception e)
+                {
+                    // fail silent
+                }
+            }
+        }
+
+    }
+}
